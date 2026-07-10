667f3c905354324ad3dd7dd1bab175517bdc1e37
dandanhua1 <2371068594@qq.com>
2026-07-10 14:59:01 +0800

    更新school前后端密码

--- school/backend/score-server/server.js
+++ school/backend/score-server/server.js
@@ -1,27 +1,53 @@
-const express = require('express')
-const mysql = require('mysql2/promise')
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
 
-const pool = mysql.createPool({
-  host: 'localhost',
+const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: '119.254.168.121',
+  port: 3306,
   user: 'root',
-  password: '',
-  database: 'school',
-  port: 3306
-})
+  password: '20050509',
+  database: 'school'
+});
 
-app.get('/api/score', async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM student_score')
-    res.json(rows)
-  } catch (err) {
-    res.status(500).json({ msg: '数据库读取失败', err })
+db.connect((err) => {
+  if (err) {
+    console.log('数据库连接失败：', err);
+    return;
   }
-})
+  console.log('本地数据库连接成功');
+});
 
-const port = 4000
-app.listen(port, () => {
-  console.log(`后端服务运行：http://localhost:${port}`)
-})
+
+app.get('/api/score', (req, res) => {
+  console.log("接受到请求。。。")
+  db.query('SELECT * FROM student_score', (err, rows) => {
+    if (err) {
+      res.status(500).json({ msg: '成绩查询失败' });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.get('/api/getStudent', (req, res) => {
+  // const sql = `
+  //   SELECT s.student_id, student_name, course_name, score, exam_time
+  //   FROM student st
+  //   JOIN student_score s ON st.student_id = s.student_id
+  // `;
+  // db.query(sql, (err, rows) => {
+  //   if (err) {
+  //     res.status(500).json({ msg: '联表查询失败' });
+  //     return;
+  //   }
+  //   res.json(rows);
+  // });
+});
+
+app.listen(3001, () => {
+  console.log('服务运行在 localhost:3001');
+});
